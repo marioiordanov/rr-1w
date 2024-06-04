fda80e4c7c208c65f14f18f87c65625f535b4e6d
marioiordanov <mio@shardlabs.io>
2024-06-04 19:00:52 +0300

    erc with sanctions

---
+++ src/ERC20WithSanctions.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.20;
+import {ERC20} from "@openzeppelin/contracts@v5.0.2/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts@v5.0.2/access/Ownable.sol";
+
+/// @title ERC20 contract which allows the owner to sanction addresses to send and receive tokens
+/// @author marioiordanov
+/// @notice Ordinary user can use this contract for basic token functionality, but the owner can also stop you from sending and receiving tokens
+/// @dev s_sanctionedAddresses is a mapping holding the sanctioned addresses
+contract ERC20WithSanctions is ERC20, Ownable {
+    uint256 private constant INITIAL_OWNER_BALANCE = 1 ether;
+    // state vars
+    mapping(address => bool) private s_sanctionedAddresses;
+
+    // events
+    event AddressSanctioned(address indexed account);
+    event AddressIsNotSanctioned(address indexed account);
+
+    // errors
+    error SanctionedSender();
+    error SanctionedReceiver();
+
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC20(name, symbol) Ownable(msg.sender) {
+        _mint(owner(), INITIAL_OWNER_BALANCE);
+    }
+
+    function sanctionAddress(address account) external onlyOwner {
+        s_sanctionedAddresses[account] = true;
+        emit AddressSanctioned(account);
+    }
+
+    function removeSanctionForAddress(address account) external onlyOwner {
+        s_sanctionedAddresses[account] = false;
+        emit AddressIsNotSanctioned(account);
+    }
+
+    function mint(address account, uint256 amount) external onlyOwner {
+        _mint(account, amount);
+    }
+
+    function transfer(
+        address to,
+        uint256 value
+    ) public override returns (bool) {
+        address msgSender = _msgSender();
+        if (isAddressSanctioned(msgSender)) {
+            revert SanctionedSender();
+        }
+
+        if (isAddressSanctioned(to)) {
+            revert SanctionedReceiver();
+        }
+
+        super._transfer(msgSender, to, value);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) public override returns (bool) {
+        if (isAddressSanctioned(from)) {
+            revert SanctionedSender();
+        }
+
+        if (isAddressSanctioned(to)) {
+            revert SanctionedReceiver();
+        }
+
+        _spendAllowance(from, msg.sender, value);
+        _transfer(from, to, value);
+        return true;
+    }
+
+    function isAddressSanctioned(address account) public view returns (bool) {
+        return s_sanctionedAddresses[account];
+    }
+}
---
+++ test/ERC20WithSanctionsTest.t.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {ERC20WithSanctions} from "../src/ERC20WithSanctions.sol";
+
+contract ERC20WithSanctionsTest is Test {
+    ERC20WithSanctions private erc20;
+    address private OWNER = address(1);
+    address private SANCTIONED = address(2);
+    address private RANDOM_USER = address(3);
+
+    function setUp() public {
+        vm.prank(OWNER);
+        erc20 = new ERC20WithSanctions("Sanctions", "STT");
+
+        vm.startPrank(OWNER);
+        erc20.mint(SANCTIONED, 1 ether);
+        erc20.mint(RANDOM_USER, 1 ether);
+        vm.stopPrank();
+    }
+
+    modifier withSaction() {
+        vm.prank(OWNER);
+        erc20.sanctionAddress(SANCTIONED);
+        _;
+    }
+
+    function testSanctionedUserCantTransferTokens() public withSaction {
+        vm.prank(SANCTIONED);
+        vm.expectRevert(ERC20WithSanctions.SanctionedSender.selector);
+        erc20.transfer(RANDOM_USER, 1 ether);
+    }
+
+    function testSanctionedUserCantTransferFromTokens() public withSaction {
+        vm.prank(SANCTIONED);
+        erc20.approve(RANDOM_USER, type(uint256).max);
+
+        vm.prank(RANDOM_USER);
+        vm.expectRevert(ERC20WithSanctions.SanctionedSender.selector);
+        erc20.transferFrom(SANCTIONED, RANDOM_USER, 1 ether);
+    }
+
+    function testOnlyOwnerCanSanctionAddress() public {
+        vm.prank(RANDOM_USER);
+        vm.expectRevert();
+        erc20.sanctionAddress(SANCTIONED);
+        assertEq(erc20.isAddressSanctioned(SANCTIONED), false);
+
+        vm.prank(OWNER);
+        erc20.sanctionAddress(SANCTIONED);
+        assertEq(erc20.isAddressSanctioned(SANCTIONED), true);
+    }
+
+    function testSanctionedUserCantReceiveTokensViaTransfer()
+        public
+        withSaction
+    {
+        vm.prank(RANDOM_USER);
+        vm.expectRevert(ERC20WithSanctions.SanctionedReceiver.selector);
+        erc20.transfer(SANCTIONED, 1 ether);
+    }
+
+    function testSanctionedUserCantReceiveTokensViaTransferFrom()
+        public
+        withSaction
+    {
+        vm.prank(OWNER);
+        erc20.approve(RANDOM_USER, type(uint256).max);
+
+        vm.prank(RANDOM_USER);
+        vm.expectRevert(ERC20WithSanctions.SanctionedReceiver.selector);
+        erc20.transferFrom(OWNER, SANCTIONED, 1 ether);
+    }
+}
